d67f49553e869b04bc99f1dde8a0dd0787de9e5a
MeitalShpigel14 <122394976+MeitalShpigel14@users.noreply.github.com>
2024-09-01 15:52:11 +0300

    Add files via upload

---
+++ src/Home.js
@@ -0,0 +1,69 @@
+import React from "react";
+import "./Home.css"; // Import the CSS file for styling
+import logo from './logo.png';
+
+
+const Home = () => {
+  return (
+    <div className="home-container">
+      <h1>Welcome to ProtectHerChat</h1>
+      <h2>Empowering Safer Surfing for Your Girls!</h2>
+
+      <div className="image-section">
+        {/* You can add images here */}
+        {/* Example: <img src="your-image-url.jpg" alt="Description" /> */}
+      </div>
+
+      <p>
+        Discover our chatbot – the one-stop hub for parents and professionals
+        dedicated to safeguarding girls in the digital realm.
+      </p>
+
+      {/* Add more image sections or other content as needed */}
+
+      <h2>Advanced Features for Enhanced Safety</h2>
+
+      <p>
+        The site includes a chatbot that will help you get information on issues
+        in the areas of online safety, dangerous online behaviors, how to help
+        girls maintain privacy, how to identify if a girl is in mental distress,
+        and even how to deal with online bullying.
+      </p>
+
+  
+
+      <p>
+        Moreover, our website offers advanced features to enhance safety. It can
+        analyze online profiles for suspicious activity, providing valuable
+        insights to parents. Furthermore, our platform scans text content for
+        potential offensive material. This ensures proactive monitoring of
+        websites and messages, safeguarding girls from harmful online
+        interactions.
+      </p>
+
+      <h2>Comprehensive Resources and Support</h2>
+
+      <p>
+        Additionally, our website provides comprehensive resources, including
+        information on associations and support centers, should you require
+        professional assistance. We're committed to connecting users with the
+        necessary support networks to address any concerns or challenges they
+        may encounter.
+      </p>
+
+      <p>
+        Our goal is to create a safer virtual environment for girls, with the
+        ultimate aspiration that the real world remains a safe haven for all
+        girls and women. Through our dedication and efforts, we strive to foster
+        a digital landscape where girls can thrive without fear, mirroring the
+        safety and security they deserve in their everyday lives.
+      </p>
+      <div className="logo">
+        <img src={logo} alt="ProtectHer" className="logo" />
+      </div>
+      <h2>Wishing you a pleasant and secure browsing experience. Stay safe and take care!</h2>
+    </div>
+  );
+};
+
+export default Home;
